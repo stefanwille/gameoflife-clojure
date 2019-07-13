763c49d10294d6e2c2900463d7f36481764ae0dd
Stefan Wille <github@stefanwille.com>
2019-07-13 09:43:43 +0200

    Solution

---
+++ README.md
@@ -0,0 +1,20 @@
+# Game of Life
+
+Game of Life in Clojure.
+
+All code is in `test/gameoflife/core_test.clj`.
+
+## Usage
+
+Run via
+
+```bash
+$ lein test
+```
+
+or
+
+```bash
+$ lein auto test
+```
+
---
+++ test/gameoflife/core_test.clj
@@ -0,0 +1,74 @@
+(ns gameoflife.core-test
+  (:require [clojure.test :refer :all]
+            [gameoflife.core :refer :all]))
+
+
+(defn cell-at [board x y]
+  (let [row (get board y)
+        cell (get row x)
+        cell-string (str cell)]
+    (if (= cell-string "") " " cell-string)))
+
+(defn neighbours [board x y]
+  [(cell-at board (+ x -1) (+ y -1))
+   (cell-at board (+ x 0) (+ y -1))
+   (cell-at board (+ x -1) (+ y -1))
+   (cell-at board (+ x +1) (+ y 0))
+   (cell-at board (+ x -1) (+ y 0))
+   (cell-at board (+ x -1) (+ y +1))
+   (cell-at board (+ x 0) (+ y +1))
+   (cell-at board (+ x +1) (+ y +1))])
+
+(defn neighbour-count [board x y] 0
+  (count (filter #(= "O" %) (neighbours board x y))))
+
+(defn next-cell [board x y]
+  (let [live-neighbours (neighbour-count board x y)
+        is-live-cell (= "O" (cell-at board x y))
+        is-dead-cell (not is-live-cell)]
+    (cond
+      (and is-live-cell (< live-neighbours 2)) " "
+      (and is-live-cell (or (= live-neighbours 2) (= live-neighbours 3))) "O"
+      (and is-live-cell (> live-neighbours 3)) " "
+      (and is-dead-cell (= live-neighbours 3)) "O"
+      (and is-dead-cell (not= live-neighbours 3)) " "
+      :else "UNEXPECTED CASE")))
+
+(deftest cell-at-test
+  (let [board ["  " " O " "O  "]]
+    (testing "returns the cell at the given coordinates"
+      (is (= " " (cell-at board 0 0)))
+      (is (= "O" (cell-at board 1 1))))
+    (testing "returns an empty cell when the coordinates lie outside the board"
+      (is (= " " (cell-at board -1 0)))
+      (is (= " " (cell-at board 0 -1)))
+      (is (= " " (cell-at board 5 0)))
+      (is (= " " (cell-at board 0 5))))))
+
+(deftest neighbours-test
+  (testing
+   (let [board [" O " " O " "O  "]]
+     (is (= [" "  "O"  " "  " "  " "  "O" " " " "] (neighbours board 1 1))))))
+
+(deftest neighbours-count-test
+  (testing "returns the number of neighbours of a given cell"
+    (is (= 0 (neighbour-count ["  " " O " "   "] 1 1)))
+    (is (= 1 (neighbour-count ["  " " OO" "   "] 1 1)))))
+
+(deftest next-cell-test
+  (testing "Any live cell with fewer than two live neighbours dies, as if by underpopulation"
+    (is (= " " (next-cell ["   " " O " "   "] 1 1)))
+    (is (= " " (next-cell ["   " " OO" "   "] 1 1))))
+  (testing "Any live cell with two or three live neighbours lives on to the next generation"
+    (is (= "O" (next-cell ["   " "OOO" "   "] 1 1)))
+    (is (= "O" (next-cell [" O " "OOO" "   "] 1 1))))
+  (testing "Any live cell with more than three live neighbours dies, as if by overpopulation"
+    (is (= " " (next-cell ["OO " "OOO" "   "] 1 1)))
+    (is (= " " (next-cell ["OOO" "OOO" "   "] 1 1))))
+  (testing "Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction"
+    (is (= "O" (next-cell [" O " "O O" "   "] 1 1))))
+  (testing "Any dead cell with not exactly three live neighbours stays a dead cell"
+    (is (= " " (next-cell ["   " "   " "   "] 1 1)))
+    (is (= " " (next-cell ["   " "  O" "   "] 1 1)))
+    (is (= " " (next-cell ["OO " "O O" "   "] 1 1)))
+    (is (= " " (next-cell ["OOO" "O O" "   "] 1 1)))))
